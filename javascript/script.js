81d23c426bde70498b5b96cd2677b95477a69836
kilian frederix <kilianfrederix@gmail.com>
2023-12-11 07:57:39 +0100

    pagina's aangepast

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,50 @@
-// scroll function om uit de header te scrollen
+"use strict";
 
-$(document).ready(function() {
-    $('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if (target.lengt) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top
-            }, 500);
-        }
-    });
-});
+let geselecteerdekamer;
+let kamers;
+let voornaam;
+
+let allescorrect = true;
+
+function controleervoornaam() {
+    if (voornaam.lengt < 2) {
+        document.getElementById("voornaam_error").innerHTML = "Minstens 2 karakters ingeven!";
+        allescorrect = false;
+    } else {
+        document.getElementById("voornaam_error").innerHTML = "";
+    }
+}
+
+
+
+
+function verstuur() {
+    geselecteerdekamer = document.getElementById("kamers").selectedIndex;
+    kamers = document.getElementById("kamers").value;
+    voornaam = document.getElementById("voornaam").value;
+
+    // controle voornaam 
+    if (voornaam.lengt == 0) {
+        document.getElementById("voornaam_error").innerHTML = "Vul je naam in";
+        allescorrect = false;
+    } else {
+        controleervoornaam()
+    }
+
+    // controle geselcteerde kamers
+    if (geselecteerdekamer == 0) {
+        document.getElementById("kamers_error").innerHTML = "Kies een optie!!";
+        allescorrect = false;
+    } else {
+        document.getElementById("kamers_error").innerHTML = "";
+    }
+
+
+
+
+
+
+
+
+}
+verstuur()
